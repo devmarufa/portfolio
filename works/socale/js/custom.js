a4b4d367d668f2823222013021e9ca291860676d
Dev Marufa <58261203+devmarufa@users.noreply.github.com>
2020-06-13 11:00:52 +0600

    Add files via upload

---
+++ works/socale/js/custom.js
@@ -0,0 +1,12 @@
+(function ($) {
+	"use strict"; // starts of use strict
+$('.one-time').slick({
+   dots: true,
+   arrows: false,
+   autoplay: true,
+   infinite: true,
+   speed: 300,
+   slidesToShow: 1,
+   adaptiveHeight: true
+  });
+})(jQuery); //end of use strict
