0dd0c3e6fbfb5edbd63714809ecbeebc2302c656
Dev Marufa <58261203+devmarufa@users.noreply.github.com>
2020-06-20 17:35:21 +0600

    Add files via upload

---
+++ works/chaty/Petclub/js/custom.js
@@ -0,0 +1,33 @@
+(function ($) {
+    "use strict"; // starts of use strict
+
+    // slider js starts here
+
+
+    $('.one-time').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1
+    });
+
+    // slider js ends here
+    $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        arrows: true
+    });
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.slider-nav'
+    });
+
+
+
+
+})(jQuery); //end of use strict
